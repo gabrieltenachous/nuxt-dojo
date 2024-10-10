cabcd4dbecb3192bcef06448fa9d1ce703006c75
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2024-10-10 07:15:44 -0300

    feching data

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem quod tempora beatae, id tempore ab at, suscipit tenetur totam, vero sunt expedita voluptas quisquam minus vitae iure libero illo?</p>
+    </div>
+</template>
+<script setup> 
+</script>
+<style scoped>
+    h2 {
+        margin-bottom: 20px; 
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
